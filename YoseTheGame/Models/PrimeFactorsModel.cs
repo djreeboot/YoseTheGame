333ec91305795c24d20d3b3784c0bfeaf0e6dfd2
djreeboot <bchampagne@victrix.ca>
2014-06-03 16:03:24 -0400

    PrimeFactors

---
+++ YoseTheGame/Models/PrimeFactorsModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YoseTheGame.Models
+{
+    public class PrimeFactorsModel
+    {
+        public int number { get; set; }
+        public List<int> decomposition { get; set; }
+
+        public void Decompose()
+        {
+            decomposition = new List<int>();
+            int temp = number;
+            while (temp != 1)
+            {
+                temp /= 2;
+                decomposition.Add(2);
+            }
+        }
+    } 
+
+}
