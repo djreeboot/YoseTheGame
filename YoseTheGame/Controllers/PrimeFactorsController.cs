ac5cf93f32e2703691e4589a9880c27f518e4e11
djreeboot <bchampagne@victrix.ca>
2014-06-03 16:04:11 -0400

    PrimeFactors

---
+++ YoseTheGame/Controllers/PrimeFactorsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YoseTheGame.Models;
+
+namespace YoseTheGame.Controllers
+{
+    public class PrimeFactorsController : Controller
+    {
+        //
+        // GET: /PrimeFactors/
+
+        public ActionResult Index(string number)
+        {
+            PrimeFactorsModel model = new PrimeFactorsModel();
+            model.number = int.Parse(number);
+            model.Decompose();
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
