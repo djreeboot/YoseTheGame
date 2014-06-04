d70f3f41be1c8d6a2e2ee62862c540d089790ae9
djreeboot <bchampagne@victrix.ca>
2014-06-04 16:27:49 -0400

    Latest

---
+++ YoseTheGame/Models/BigNumberResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YoseTheGame.Models
+{
+    public class BigNumberResponse : Response
+    {
+        public int number { get; set; }
+
+        public string error { get; set; }
+    }
+}
