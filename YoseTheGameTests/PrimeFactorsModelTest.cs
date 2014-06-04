805faaa5df88b7ced17b6cf866aca03c4ec320a0
djreeboot <bchampagne@victrix.ca>
2014-06-04 10:58:41 -0400

    String Guard

--- YoseTheGameTests/PrimeFactorsModelTest.cs
+++ YoseTheGameTests/PrimeFactorsModelTest.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Web.Mvc;
+using NUnit.Framework;
+using YoseTheGame.Controllers;
+using YoseTheGame.Models;
 
 namespace YoseTheGameTests
 {
-    class PrimeFactorsModelTest
+    [TestFixture]
+    public class PrimeFactorsModelTest
     {
+        private NumberResponse response = null;
+
+        [SetUp]
+        public void ThisModel()
+        {
+            PrimeFactorsModel model = new PrimeFactorsModel();
+            response = (NumberResponse)model.Decompose("16");
+        }
+
+        [Test]
+        public void ReturnsSpecifiedNumber()
+        {
+            Assert.That(response.number, Is.EqualTo(16));
+        }
+
+        [Test]
+        public void ReturnsValidDecomposition()
+        {
+            Assert.That(response.decomposition, Is.EqualTo(new List<int> { 2, 2, 2, 2 }));
+        }
     }
 }
