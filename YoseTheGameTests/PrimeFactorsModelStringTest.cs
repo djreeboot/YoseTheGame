a54c02c5c38ae9d921d1e13ba7dcee160bf58622
djreeboot <bchampagne@victrix.ca>
2014-06-04 10:58:17 -0400

    String Guard

---
+++ YoseTheGameTests/PrimeFactorsModelStringTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using NUnit.Framework;
+using YoseTheGame.Controllers;
+using YoseTheGame.Models;
+
+namespace YoseTheGameTests
+{
+    [TestFixture]
+    public class PrimeFactorsModelStringTest
+    {
+        private ErrorResponse response = null;
+
+        [SetUp]
+        public void ThisModel()
+        {
+            PrimeFactorsModel model = new PrimeFactorsModel();
+            response = (ErrorResponse)model.Decompose("batman");
+        }
+
+        [Test]
+        public void ReturnsSpecifiedNumber()
+        {
+            Assert.That(response.number, Is.EqualTo("batman"));
+        }
+
+        [Test]
+        public void ReturnsExpectedError()
+        {
+            Assert.That(response.error, Is.EqualTo("not a number"));
+        }
+    }
+}
