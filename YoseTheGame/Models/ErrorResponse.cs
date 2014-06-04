c25532f1fb61075f42e7970071d2a72e65e84552
djreeboot <bchampagne@victrix.ca>
2014-06-04 11:03:33 -0400

    String Guard

---
+++ YoseTheGame/Models/ErrorResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YoseTheGame.Models
+{
+    public class ErrorResponse : Response
+    {
+        public string number { get; set; }
+        public string error { get; set; }
+    }
+}
